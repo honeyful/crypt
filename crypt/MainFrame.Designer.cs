1cad8018482650123ef4e89fe7b58ff1ab0593c2
honeyful <honeyful@users.noreply.github.com>
2017-09-06 17:22:04 +0900

    init

---
+++ crypt/MainFrame.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace crypt
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtPassKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 305);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(156, 23);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(174, 305);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(156, 23);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(156, 260);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.Text = "INPUT";
+            this.txtInput.Click += new System.EventHandler(this.txtInput_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(174, 12);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(156, 260);
+            this.txtOutput.TabIndex = 3;
+            this.txtOutput.Text = "OUTPUT";
+            this.txtOutput.Click += new System.EventHandler(this.txtOutput_Click);
+            // 
+            // txtPassKey
+            // 
+            this.txtPassKey.Location = new System.Drawing.Point(12, 278);
+            this.txtPassKey.Name = "txtPassKey";
+            this.txtPassKey.Size = new System.Drawing.Size(318, 21);
+            this.txtPassKey.TabIndex = 4;
+            this.txtPassKey.Text = "PassKey";
+            this.txtPassKey.Click += new System.EventHandler(this.txtPassKey_Click);
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 332);
+            this.Controls.Add(this.txtPassKey);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "MainFrame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtPassKey;
+    }
+}
+
---
+++ crypt/MainFrame.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace crypt
+{
+    public partial class MainFrame : Form
+    {
+        private readonly string saltKey = "puvZU9eT4puvZUal0HJs8t3";
+        private readonly string viKey = "mSbuxJ42lK8URHKh";
+
+
+        private string Encrypt(string plainText)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            byte[] keyBytes = new Rfc2898DeriveBytes(txtPassKey.Text, Encoding.ASCII.GetBytes(saltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+            var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(viKey));
+
+            byte[] cipherTextBytes;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                    cryptoStream.FlushFinalBlock();
+                    cipherTextBytes = memoryStream.ToArray();
+                    cryptoStream.Close();
+                }
+                memoryStream.Close();
+            }
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+
+        private string Decrypt(string encryptedText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+            byte[] keyBytes = new Rfc2898DeriveBytes(txtPassKey.Text, Encoding.ASCII.GetBytes(saltKey)).GetBytes(256 / 8);
+            var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+            var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(viKey));
+            var memoryStream = new MemoryStream(cipherTextBytes);
+            var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+        }
+
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPassKey_Click(object sender, EventArgs e)
+        {
+            txtPassKey.SelectAll();
+        }
+
+        private void txtInput_Click(object sender, EventArgs e)
+        {
+            txtInput.SelectAll();
+        }
+
+        private void txtOutput_Click(object sender, EventArgs e)
+        {
+            txtOutput.SelectAll();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = Encrypt(txtInput.Text);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = Decrypt(txtInput.Text);
+        }
+    }
+}
